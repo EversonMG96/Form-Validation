70006842e404a2ea4f56047e50a49ed3404a418e
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-22 12:42:58 -0300

    create news vars inthe main .js add id in the html

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,3 +3,4 @@ var phoneError = document.getElementById("phone-error");
 var emailError = document.getElementById("email-error");
 var messageError = document.getElementById("message-error");
 var submitError = document.getElementById("submit-error");
+
