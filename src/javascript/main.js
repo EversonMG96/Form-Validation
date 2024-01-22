65cc0b67f9cef1f8611e9fe2d18161e31d065f45
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-22 12:51:32 -0300

    creat var and structure of control in the function validateName

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -5,5 +5,9 @@ var messageError = document.getElementById("message-error");
 var submitError = document.getElementById("submit-error");
 
 function validateName() {
-    var name = document.getElementById("contact-name");
+    var name = document.getElementById("contact-name").value;
+
+    if(name.length == 0) {
+        nameError.innerHTML = "Name is required";
+    }
 }
