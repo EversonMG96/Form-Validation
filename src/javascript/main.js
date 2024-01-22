b167e7ba80536b7f4cc6e7de07ae9673d5df59a3
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-22 12:59:34 -0300

    add return for the structures of control

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -9,8 +9,10 @@ function validateName() {
 
     if(name.length == 0) {
         nameError.innerHTML = "Name is required";
+        return false;
     }
     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
         nameError.innerHTML = "Write full name";
+        return false;
     }
 }
